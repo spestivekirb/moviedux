8d606b3372657e680608f47783e63698dbfbe25c
Jerry Wong <187459546+spestivekirb@users.noreply.github.com>
2026-02-28 14:02:54 -0500

    Add events: filters and search logic.

--- src/components/MoviesGrid.js
+++ src/components/MoviesGrid.js
@@ -1,11 +1,14 @@
 import React, {useState, useEffect} from 'react';
 import '../styles.css';
+import MovieCard from './MovieCard';
 
 export default function MoviesGrid() {
     
     const [movies, setMovies] = useState([]);
-    
-    
+    const [searchTerm, setSearchTerm] = useState("");
+
+    const [genre, setGenre] = useState("All Genres");
+    const [rating, setRating] = useState("All");
 
     useEffect(() => {
         
@@ -15,20 +18,94 @@ export default function MoviesGrid() {
 
     }, [])
 
+    const handleSearchChange = (e) => {
+        setSearchTerm(e.target.value);
+    };
+
+    const handleGenreChange = (e) => {
+        setGenre(e.target.value);
+    };
+
+    const handleRatingChange = (e) => {
+        setRating(e.target.value);
+    };
+
+    const matchesGenre = (movie, genre) => {
+        return genre === "All Genres" || movie.genre.toLowerCase() === genre.toLowerCase();
+    }
+
+    const matchesSearchTerm = (movie, searchTerm) => {
+        return movie.title.toLowerCase().includes(searchTerm.toLowerCase());
+    }
+
+    const matchesRating = (movie, rating) => {
+        switch(rating) {
+            case "All":
+                return true;
+
+            case "Good":
+                return movie.rating >= 8;
+            case "Ok": 
+                return movie.rating >= 5 && movie.rating < 8;
+            case "Bad":
+                return movie.rating < 5;
+
+            default:
+                return false;
+        }
+    }
+
+    const filteredMovies = movies.filter(movie =>
+        matchesGenre(movie, genre) &&
+        matchesRating(movie, rating) &&
+        matchesSearchTerm(movie, searchTerm)
+    );
+
+
     return (
-        <div className='movies-grid'>
-            {
-                movies.map(movie => (
-                    <div key={movie.id} className='movie-card'>
-                        <img src={`images/${movie.image}`} alt={movie.title}></img>
-                        <div className = 'movie-card-info'>
-                            <h3 className='movie-card-title'>{movie.title}</h3>
-                            <p className='movie-card-genre'>{movie.genre}</p>
-                            <p className='movie-card-rating'>{movie.rating}</p>
-                        </div>
-                    </div> 
-                ))
-            }
+        <div>
+            <input
+                type="text"
+                placeholder="Search movies..."
+                className="search-input"
+                value={searchTerm}
+                onChange={handleSearchChange}
+            />
+            <div className="filter-bar">
+
+                <div className="filter-slot">
+                    <label>Genre</label>
+                    <select className="filter-dropdown" value={genre} onChange={handleGenreChange}>
+                        <option>All Genres</option>
+                        <option>Action</option>
+                        <option>Drama</option>
+                        <option>Fantasy</option>
+                        <option>Horror</option>
+                    </select>
+                </div>
+                <div className="filter-slot">
+                    <label>Rating</label>
+                    <select className="filter-dropdown" value={rating} onChange={handleRatingChange}>
+                        <option>All</option>
+                        <option>Good</option>
+                        <option>Ok</option>
+                        <option>Bad</option>
+                    </select>
+                </div>
+            </div>
+
+            
+            
+            <div className='movies-grid'>
+                {
+                    filteredMovies.map(movie => (
+                        <MovieCard movie={movie} key={movie.id}></MovieCard>
+                    ))
+                }
+            </div>
+
+
+
         </div>
     )
 }
